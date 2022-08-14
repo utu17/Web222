7fcc18e0c4534bbc4f9b77cdb78b0814cf94490b
utu17 <111216314+utu17@users.noreply.github.com>
2022-08-14 16:16:09 -0400

    Blog-utsavB
    Created a new personal blog for Utsav Bhanderi

---
+++ app.js
@@ -0,0 +1,31 @@
+/**
+ * WEB222 – Assignment 04
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       Utsav Jiteshkumar Bhanderi
+ *      Student ID: 159776202
+ *      Date:       22 july 2022
+ */
+
+// All of our data is available on the global `window` object.
+// Create local variables to work with it in this file.
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})
+();
